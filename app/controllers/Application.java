afd373e7cf7656d42917ddba375a1a970ff665ed
Fredrik Bromee <fredrik.bromee@gmail.com>
2012-04-19 08:54:02 +0200

    parse json response

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,6 +3,8 @@ package controllers;
 import play.libs.WS;
 import play.mvc.Controller;
 
+import com.google.gson.Gson;
+
 public class Application extends Controller {
 
 	public static void index(String server, String port) {
@@ -14,9 +16,16 @@ public class Application extends Controller {
 				serverUrl = serverUrl + ":" + port;
 			}
 		}
-		String url = WS.url(serverUrl).get().getString();
-		System.out.println(url);
-		render(url);
+
+		// print json and parsed result
+		String json = WS.url(serverUrl).get().getString();
+
+		Resp parsed = new Gson().fromJson(json, Resp.class);
+		int time = parsed.lastCrossed;
+		render(json, time);
     }
 
+	public final static class Resp {
+		public int lastCrossed;
+	}
 }
